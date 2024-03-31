e2198a3656f1f24d001e42b06706223a4481cd0e
Hanna-Nagrehelna <anna.nagrebelna@gmail.com>
2024-03-31 23:50:06 +0300

    Updated 1-gallery.html, 1-gallery.js, created falder images.js, updated 2-form.html, index.html

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,97 +1,14 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const images = [
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
-},
+import { images } from './images';
 
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-},
+const galleryContainer = document.querySelector('.gallery');
 
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-},
-
-{
-    preview:
-    'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-},
-];
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const galleryList = document.querySelector('.gallery-list');
-
-galleryList.addEventListener('click', handleGalleryClick);
-
-function handleGalleryClick(event) {
-  event.preventDefault();
-
-  const imageLink = event.target.closest('.gallery-link');
-  if (!imageLink) return;
-
-  const largeImageSrc = imageLink.getAttribute('href');
-  
-    // Створення екземпляру SimpleLightbox з великим зображенням
-  const lightbox = new SimpleLightbox('.gallery-link a');
-
-    // Показати модальне вікно
-  lightbox.show();
-}
-
-
-  // Окремо розмітка галереї
-  const imagesMarkup = images
-    .map(({ preview, original, description }) => `
-  <li class="gallery-item">
+function createMarkUp(images) {
+  return images
+    .map(
+      ({ preview, original, description }) => `<li class="gallery-item">
     <a class="gallery-link" href="${original}">
       <img
       class="gallery-image"
@@ -99,25 +16,19 @@ function handleGalleryClick(event) {
       alt="${description}" 
       />      
     </a>
-  </li>`)
+  </li>`
+    )
     .join('');
+}
 
-  gallery.insertAdjacentHTML('afterbegin', imagesMarkup);
+galleryContainer.innerHTML = createMarkUp(images);
 
-  const gallery = new SimpleLightbox('.gallery-list', {
-  overlay: true,
-  spinner: true,
+const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
-  close: true,
-  showCounter: true,
   captionsData: 'alt',
   captionPosition: 'bottom',
-  disableRightClick: true,
-  alertErrorMessage: 'Image not found, next image will be loaded',
-  disableScroll: true  
-  });
-
 });
+
 
 
 
